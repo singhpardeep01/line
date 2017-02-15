0c955723e33cabef73bfe334513bc41126e44478
singhpardeep01 <singhpardeep01>
2017-02-15 11:44:13 -0500

    files octant 1278

--- draw.py
+++ draw.py
@@ -1,6 +1,35 @@
 from display import *
 
-def draw_line( screen, x0, y0, x1, y1, color ):
+def draw_line( x0, y0, x1, y1, screen, color ):
+    A = y1 - y0
+    B = x0 - x1
+    if ( B > 0 ):
+        #3456
+        if ( A > 0 ):
+            #34
+        else:
+            #56
+    else:
+        #1278
+        if ( A > 0 ):
+            #12
+            if ( A < -1*B ):
+                #1
+                octant1( x0, y0, x1, y1, screen, color )
+            else:
+                #2
+                octant2( x0, y0, x1, y1, screen, color )
+        else:
+            #78
+            if ( -1*A < -1*B ):
+                #8
+                octant8( x0, y0, x1, y1, screen, color )
+            else:
+                #7
+                octant7( x0, y0, x1, y1, screen, color )
+
+
+def octant1( x0, y0, x1, y1, screen, color ):
     x = x0
     y = y0
     A = y1 - y0
@@ -11,6 +40,48 @@ def draw_line( screen, x0, y0, x1, y1, color ):
         if ( d > 0 ):
             y += 1
             d += 2*B
+        x += 1
+        d += 2*A
+    pass
+def octant2( x0, y0, x1, y1, screen, color ):
+    x = x0
+    y = y0
+    A = y1 - y0
+    B = x0 - x1
+    d = A + 2*B
+    while y <= y1:
+        plot( screen, color, x, y )
+        if ( d < 0 ):
+            x += 1
+            d += 2*A
+        y += 1
+        d += 2*B
+    pass
+def octant7( x0, y0, x1, y1, screen, color ):
+    x = x0
+    y = y0
+    A = y1 - y0
+    B = x0 - x1
+    d = A - 2*B
+    while y >= y1:
+        plot( screen, color, x, y )
+        if ( d > 0 ):
+            x += 1
+            d += 2*A
+        y -= 1
+        d -= 2*B
+    pass    
+def octant8( x0, y0, x1, y1, screen, color ):
+    x = x0
+    y = y0
+    A = y1 - y0
+    B = x0 - x1
+    d = 2*A - B
+    while x <= x1:
+        plot( screen, color, x, y )
+        if ( d < 0 ):
+            y -= 1
+            d -= 2*B
         x += 1
         d += 2*A
     pass
--- main.py
+++ main.py
@@ -3,6 +3,15 @@ from draw import *
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
-draw_line( screen, 0, 499, 300, 400, color )
+#draw_line( 0, 0, 500, 249, screen, color )
+draw_line( 250, 250, 500, 300, screen, color ) #1
+draw_line( 250, 250, 300, 500, screen, color ) #2
+draw_line( 250, 250, 300, 0, screen, color )   #7
+draw_line( 250, 250, 500, 200, screen, color ) #8
+draw_line( 250, 250, 200, 500, screen, color ) #3
+draw_line( 250, 250, 0, 300, screen, color ) #4
+draw_line( 250, 250, 0, 200, screen, color )   #5
+draw_line( 250, 250, 200, 0, screen, color ) #6
+
 display(screen)
 save_extension(screen, 'img.png')
